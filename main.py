732a580c9692700c76c4b5dd38590fcab50a1955
thomas-kw <65559171+thomas-kw@users.noreply.github.com>
2021-06-07 12:49:37 +0900

    player completed

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+turtle = Player()
+screen.listen()
+screen.onkey(turtle.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
